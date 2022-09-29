fcc26e44ae1f65fb63364fc20ccaabd3d1c4ce40
Anurag Dutt <duttanurag0005@gmail.com>
2022-09-29 12:14:16 +0530

    Day 1

---
+++ 100DAYSOFCODE/src/OddEven.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class OddEven
+{
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Enter any number ");
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+
+        if (n%2==0)
+        {
+            System.out.println("Even Number");
+        }
+        else
+        {
+            System.out.println("Odd Number");
+        }
+    }
+}
