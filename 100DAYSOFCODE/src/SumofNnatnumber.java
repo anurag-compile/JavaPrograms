fe3a9bf0fce30cd2ae231836076e634ad7a28fe7
Anurag Dutt <duttanurag0005@gmail.com>
2022-09-29 12:50:06 +0530

    Day 1

---
+++ 100DAYSOFCODE/src/SumofNnatnumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SumofNnatnumber {
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Enter No. of Term: ");
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+
+        for (int i =1; i <= n; i++)
+        {
+            System.out.println(i + " ");
+        }
+    }
+}
