001315e48a3d47b43ffe7e82a4bb627bec4287cd
Anurag Dutt <duttanurag0005@gmail.com>
2022-09-29 12:39:08 +0530

    Day 1

---
+++ 100DAYSOFCODE/src/AddTwo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class AddTwo {
+    public static void main(String[] args) {
+        int a,b,sum;
+        System.out.println("Enter two numbers: ");
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        b = in.nextInt();
+
+        sum = a+b;
+        System.out.println("Addition: " + sum);
+    }
+}
