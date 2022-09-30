927e73faa143b6ed69ba1b2eb3a340e036d23a8d
Anurag Dutt <duttanurag0005@gmail.com>
2022-09-30 21:53:44 +0530

    Day 2

---
+++ 100DAYSOFCODE/src/day3/PrintAllEven0toN.java
@@ -0,0 +1,17 @@
+package day3;
+
+import java.util.Scanner;
+
+public class PrintAllEven0toN {
+    public static void main(String[] args) {
+        int n;
+        System.out.println("Enter No. of term ");
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+
+        for (int i = 0; i <= n; i = i + 2)
+        {
+            System.out.println(i);
+        }
+    }
+}
