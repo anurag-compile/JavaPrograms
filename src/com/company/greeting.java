6ef61ecc646438b70e3c86c1974f504f40fcd49c
Anurag Dutt <duttanurag0005@gmail.com>
2022-10-03 16:15:00 +0530

    done

---
+++ src/com/company/greeting.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class greeting {
+    public static void main(String[] args) {
+        greeting();
+
+    }
+    static void greeting(){
+        System.out.println("hello world");
+    }
+}
