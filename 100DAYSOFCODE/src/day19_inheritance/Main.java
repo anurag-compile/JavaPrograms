219dc18ad26bb12fde93f4af75b01cde9b7dab05
Anurag Dutt <duttanurag0005@gmail.com>
2022-10-31 09:04:45 +0530

    DAY 19

---
+++ 100DAYSOFCODE/src/day19_inheritance/Main.java
@@ -0,0 +1,11 @@
+package day19_inheritance;
+
+// Driver class
+public class Main {
+    public static void main(String[] args) {
+        two g = new two();
+        g.print_geek();
+        g.print_for();
+        g.print_geek();
+    }
+}
