3b4512ab7157588fafd08f36f8709d91075ff76a
Anurag Dutt <duttanurag0005@gmail.com>
2022-11-16 08:11:39 +0530

    DAY 35

---
+++ 100DAYSOFCODE/src/day35_JavaDateandTime/JavaDateandTime.java
@@ -0,0 +1,46 @@
+package day35_JavaDateandTime;
+import java.io.*;
+import java.util.*;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month-1, day);
+        String[] arr = {"SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
+        return arr[cal.get(Calendar.DAY_OF_WEEK)-1];
+    }
+}
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ 100DAYSOFCODE/src/day35_JavaDateandTime/readme.md
@@ -0,0 +1,27 @@
+# Java Date and Time
+
+
+
+The Calendar class is an abstract class that provides methods for converting between a specific instant in time and a set of calendar fields such as YEAR, MONTH, DAY_OF_MONTH, HOUR, and so on, and for manipulating the calendar fields, such as getting the date of the next week.
+
+You are given a date. You just need to write the method, DAY , which returns the day on that date. To simplify your task, we have provided a portion of the code in the editor.
+
+![img.png](img.png)
+
+### Function Description
+
+Complete the findDay function in the editor below.
+
+### findDay has the following parameters:
+
+int: month
+int: day
+int: year
+Returns
+
+### string: 
+the day of the week in capital letters
+
+![img_1.png](img_1.png)
+
+![img_2.png](img_2.png)
