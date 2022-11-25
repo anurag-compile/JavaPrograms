31c396df7f0eb79ab2efefec972c147cf94fac5b
Anurag Dutt <duttanurag0005@gmail.com>
2022-11-25 19:10:47 +0530

    DAY 44

---
+++ 100DAYSOFCODE/src/day044_InheritanceProblem/solution.java
@@ -0,0 +1,31 @@
+package day044_InheritanceProblem;
+
+class Animal{
+    void walk(){
+        System.out.println("I am walking");
+    }
+}
+
+class Bird extends Animal{
+    void fly(){
+        System.out.println("I am flying");
+    }
+    void walk(){
+        System.out.println("I am walking");
+    }
+    void sing(){
+        System.out.println("I am singing");
+    }
+}
+
+public class solution{
+
+    public static void main(String[] args){
+
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+
+    }
+}
