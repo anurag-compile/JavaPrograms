08778a1a4117dd19252a0cc17e3a4ae06cdc23ea
Anurag Dutt <duttanurag0005@gmail.com>
2022-11-30 11:18:50 +0530

    DAY 49

---
+++ 100DAYSOFCODE/src/day049_JavaInterface/Solution.java
@@ -0,0 +1,42 @@
+package day049_JavaInterface;
+
+import java.util.*;
+interface AdvancedArithmetic{
+    int divisor_sum(int n);
+}
+
+//Write your code here
+class MyCalculator implements AdvancedArithmetic{
+    public int divisor_sum(int n){
+        int sum_div = 0;
+        for (int i = 1; i<= n;i++){
+            if (n % i == 0){
+                sum_div = sum_div + i;
+            }
+        }
+        return sum_div;
+    }
+}
+
+class Solution{
+    public static void main(String []args){
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(my_calculator.divisor_sum(n) + "\n");
+        sc.close();
+    }
+    /*
+     *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
+     */
+    static void ImplementedInterfaceNames(Object o){
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        for (Class theInterface : theInterfaces) {
+            String interfaceName = theInterface.getName();
+            System.out.println(interfaceName);
+        }
+    }
+}
+
