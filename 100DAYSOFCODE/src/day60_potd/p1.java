e49715e0fc954e5f72c4dba29b7519e9ba525eb2
Anurag Dutt <62209181+anurag-compile@users.noreply.github.com>
2023-01-11 19:45:43 +0530

    Day 60

---
+++ 100DAYSOFCODE/src/day60_potd/p1.java
@@ -0,0 +1,17 @@
+package day60_potd;
+import java.util.Arrays;
+class Solution {
+    public long minIncrements(int[] arr, int N) {
+        // Code here
+        Arrays.sort(arr);
+        long ans =0;
+
+        for(int i=1;i<N;i++) {
+            if(arr[i-1]>=arr[i]) {
+                ans+=(long)(arr[i-1]-arr[i]+1);
+                arr[i]=arr[i-1]+1;
+            }
+        }
+        return ans;
+    }
+}
