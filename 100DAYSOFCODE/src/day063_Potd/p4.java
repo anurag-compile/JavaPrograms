6c62eeeee42403fade4fc2d818e2ba3a439ab9b5
Anurag Dutt <62209181+anurag-compile@users.noreply.github.com>
2023-01-14 21:37:11 +0530

    done for the day :/

---
+++ 100DAYSOFCODE/src/day063_Potd/p4.java
@@ -0,0 +1,18 @@
+package day063_Potd;
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0) return "";
+        String prefix=strs[0];
+        for(int i=1;i<strs.length;i++){
+            while(strs[i].indexOf(prefix)!=0){
+                prefix=prefix.substring(0,prefix.length()-1);
+            }
+
+        }
+        return prefix;
+
+
+    }
+//Please upvote
+}
