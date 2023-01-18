2c460084e127515d234e7547d2cf1999c1c1f1e3
Anurag Dutt <62209181+anurag-compile@users.noreply.github.com>
2023-01-18 13:59:06 +0530

    Day 67

---
+++ 100DAYSOFCODE/src/day067_POTD/p8.java
@@ -0,0 +1,13 @@
+package day067_POTD;
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int c = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[c] = nums[i];
+                c++;
+            }
+        }
+        return c;
+    }
+}
